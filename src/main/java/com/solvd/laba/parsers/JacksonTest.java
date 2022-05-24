01c88e04bd68b87ed1dbf2538402d16589be4278
oleg-by <allbuk.by@gmail.com>
2022-05-24 18:40:35 +0300

    add JSON annotations

--- src/main/java/com/solvd/laba/parsers/JacksonTest.java
+++ src/main/java/com/solvd/laba/parsers/JacksonTest.java
@@ -1,6 +1,7 @@
 package com.solvd.laba.parsers;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.solvd.laba.model.Purchase;
 import org.apache.logging.log4j.LogManager;
@@ -15,7 +16,6 @@ import java.util.List;
 
 public class JacksonTest {
     private static final Logger LOGGER = LogManager.getLogger(JacksonTest.class);
-
     public static void main(String[] args) throws ParseException {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
@@ -26,21 +26,24 @@ public class JacksonTest {
                     , new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2021-02-19 19:45:00"), 2000.0F));
             purchases.add(new Purchase(3, "IPhone 13 PRO"
                     , new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2022-05-10 10:55:00"), 2400.0F));
-
             UserTest userTest = new UserTest(1, "Oleg", "oleg@mail.ru"
                     , new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2022-05-10 10:55:00")
                     , new SimpleDateFormat("yyyy-MM-dd").parse("1990-07-20"), purchases);
-
-            objectMapper.writeValue(new File(System.getProperty("user.dir") + "/src/main/resources/user.json"), userTest);
-            UserTest u = objectMapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/user.json"), UserTest.class);
-            LOGGER.info(u);
-
+            File file = new File(System.getProperty("user.dir") + "/src/main/resources/user.json");
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            objectMapper.writeValue(file, userTest);
+            JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, UserTest.class);
+            List<UserTest> userTestList = objectMapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/users.json"), type);
+            //UserTest u = objectMapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/user.json"), UserTest.class);
+            for(UserTest u : userTestList){
+                LOGGER.info(u);
+            }
         } catch (JsonParseException e) {
             LOGGER.error(e.getMessage());
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
         }
-
     }
-
 }
